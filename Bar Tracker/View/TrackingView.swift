fbe57fc05dfcced5d2dbcb63eab5aedd02044825
피수영 <vltndud123@naver.com>
2021-06-30 08:54:19 +0900

    영상 로드 및 Tracking view full screen modal 구현

--- Bar Tracker/View/TrackingView.swift
+++ Bar Tracker/View/TrackingView.swift
@@ -6,18 +6,37 @@
 //
 
 import SwiftUI
+import AVFoundation
+import AVKit
 
 struct TrackingView: View {
     @Environment(\.presentationMode) var presentationMode
+    @StateObject var trackingViewModel = TrackingViewModel()
+    var videoAsset: AVAsset
+    
+    init(videoAsset: AVAsset) {
+        self.videoAsset = videoAsset
+    }
+    
     var body: some View {
         VStack {
-            
+            HStack {
+                Button {
+                    self.presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Text("Back")
+                }
+                .padding()
+                Spacer()
+            }
+            Spacer()
+            VideoPlayer(player: AVPlayer(playerItem: AVPlayerItem(asset: self.videoAsset)))
         }
     }
 }
 
 struct TrackingView_Previews: PreviewProvider {
     static var previews: some View {
-        TrackingView()
+        TrackingView(videoAsset: AVAsset())
     }
 }
---
+++ Bar Tracker/ViewModel/TrackingViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  TrackingViewModel.swift
+//  Bar Tracker
+//
+//  Created by 피수영 on 2021/06/29.
+//
+
+import Foundation
+import AVFoundation
+import Photos
+
+class TrackingViewModel: ObservableObject {
+    
+    
+}
